bd21312ade15561d1f278544a807613d1cb6c723
CoinFormalizer <CoinFormalizer@users.noreply.github.com>
2016-11-07 23:57:52 -0500

    Plugged interpreter on top of typing branch; successfully evaluates factorial.

--- tezos.v
+++ tezos.v
@@ -52,6 +52,28 @@ Inductive tagged_data:=
 (* | Contract <type> <type> <contract constant> *)
 (* | Lambda <type> <type> { <instruction> ... } *)
 
+Definition is_comparable (d : tagged_data) : bool :=
+  match d with
+    | Int8 z  => true
+    | Int64 i => true
+    | Dtrue | Dfalse => true
+    | DTez t => true
+    | _ => false
+  end.
+
+Definition is_bool d :=
+  match d with
+    | Dtrue => true
+    | Dfalse => true
+    | _ => false
+  end.
+
+Definition is_int i :=
+  match i with
+    | Int8 _ => true
+    | Int64 _ => true
+    | _ => false
+  end.
 
 Definition stack := list tagged_data.
 
@@ -69,6 +91,7 @@ Inductive instr :=
 | Dup : instr
 | Swap : instr
 | Push : tagged_data -> instr
+| Pair : instr
 | Eq : instr
 | Neq : instr
 | Lt : instr
@@ -125,6 +148,7 @@ Inductive instr_type :=
 | Arrow : list type -> list type -> instr_type
 
 with type :=
+| t_int8 : type
 | t_int64 : type
 (*| t_void : type*)
 | t_bool : type
@@ -209,6 +233,7 @@ with has_data_type : tagged_data -> type -> Prop :=
 | T_boolT : Dtrue :d: t_bool
 | T_boolF : Dfalse :d: t_bool
 | T_Int64 : forall z, Int64 z :d: t_int64
+| T_Int8 : forall z, Int8 z :d: t_int8
 where "d ':d:' DT" := (has_data_type d DT).
 
 Hint Constructors has_instr_type.
@@ -352,3 +377,323 @@ Proof. by typecheck_program. Qed.
 *)
 
 End Typechecking_Tests.
+
+Section Semantics.
+
+Axiom memory : Type.
+
+(* Second version: with a step function *)
+Section Fun_semantics.
+
+(* I'm guessing these will be replaced by accesses to memory, with a
+precise spec *)
+Axiom get_timestamp : unit -> timestamp.
+Axiom get_current_amount : unit -> tez.
+
+(* these axioms to model the behavior of Transfer_funds, which I do
+not understand as of now *)
+Axiom get_new_global : tagged_data -> tagged_data.
+Axiom get_return_value : tagged_data -> tagged_data.
+
+Axiom get_le : tagged_data -> Z.
+
+Definition tbool_of_bool b :=
+  match b with
+    | true => Dtrue
+    | false => Dfalse
+  end.
+
+Definition get_eq x : tagged_data :=
+  match x with
+    | Int8 z => tbool_of_bool (Zeq_bool z 0)
+    | Int64 z => tbool_of_bool (Zeq_bool z 0)
+    | _ => Dfalse
+  end.
+
+Definition get_neq x : tagged_data :=
+  match x with
+    | Int8 z => tbool_of_bool (negb (Zeq_bool z 0))
+    | Int64 z => tbool_of_bool (negb (Zeq_bool z 0))
+    | _ => Dfalse
+  end.
+Axiom get_lt : tagged_data -> Z.
+
+
+
+Definition get_mul (x : tagged_data) (y : tagged_data) : option tagged_data :=
+  match x,y with
+    | Int8 x,Int8 y => Some(Int8 (x * y))
+    | Int64 x,Int64 y => Some(Int64 (x * y))
+    | _,_ => None
+  end.
+
+Definition get_sub (x : tagged_data) (y : tagged_data) : option tagged_data :=
+  match x,y with
+    | Int8 x,Int8 y => Some(Int8 (x - y))
+    | Int64 x,Int64 y => Some(Int64 (x - y))
+    | _,_ => None
+  end.
+
+Fixpoint step_fun (i : instr) (s : stack) (m : memory) : option (instr * stack * memory) :=
+  match i with
+  | Nop ;; i2 => step_fun i2 s m
+  | i1 ;; i2 => if step_fun i1 s m is Some(i,s',m') then
+                   Some(i;;i2,s',m')
+                 else
+                   None
+  | Nop => Some(Nop,s,m)
+  | If bt bf => if s is x::s then
+                 match x with
+                   | Dtrue => Some(bt,s,m)
+                   | Dfalse => Some(bf,s,m)
+                   | _ => None
+                 end else None
+  | Loop body => if s is x::s then
+                  match x with
+                  | Dtrue => Some(body ;;(Loop body),s,m)
+                  | Dfalse => Some(Nop,s,m) (* doubtful *)
+                  | _ => None
+                  end else None
+  | Dip i1 => if s is x::s then
+                     match step_fun i1 s m with
+                       | Some (i2,s',m') => Some(i2,x::s',m')
+                       | None => None
+                     end else None
+  | Drop => if s is x::xs then Some(Nop,xs,m) else None
+  | Dup => if s is x::xs then Some(Nop,x::x::xs,m) else None
+  | Swap => if s is x1::x2::s then Some(Nop,x2::x1::s,m) else None
+  | Push d => Some(Nop,d::s,m)
+  | Pair => if s is a::b::s then Some(Nop,(DPair a b)::s,m) else None
+  | Eq => if s is x::s then if is_comparable x then Some(Nop,((get_eq x))::s,m) else None else None
+  | Neq => if s is x::s then if is_comparable x then Some(Nop,((get_neq x))::s,m) else None else None
+  | Lt => if s is x::s then if is_comparable x then Some(Nop,(Int64 (get_lt x))::s,m) else None else None
+  | Not => if s is x::s then match x with
+                              | Dtrue => Some(Nop,Dfalse::s,m)
+                              | Dfalse => Some(Nop,Dtrue::s,m)
+                              | _ => None
+                            end else None
+  | And => if s is x1::x2::s then
+            if (is_bool x1 && is_bool x2) then
+              match (x1,x2) with
+                | (Dtrue,Dtrue) => Some(Nop,Dtrue::s,m)
+                | (_,_) => Some(Nop,Dfalse::s,m)
+              end else None
+          else None
+  | Or => if s is x1::x2::s then
+            if (is_bool x1 && is_bool x2) then
+              match (x1,x2) with
+                | (Dtrue,_) => Some(Nop,Dtrue::s,m)
+                | (_,Dtrue) => Some(Nop,Dtrue::s,m)
+                | (_,_) => Some(Nop,Dfalse::s,m)
+              end else None
+          else None
+  | Mul => if s is x1::x2::s then
+            match (get_mul x1 x2) with
+              | Some(x12) => Some(Nop,x12::s,m)
+              | None => None
+          end else None
+  | Sub => if s is x1::x2::s then
+            match (get_sub x1 x2) with
+              | Some(x12) => Some(Nop,x12::s,m)
+              | None => None
+          end else None
+
+end.
+
+(* Fixpoint step_fun (i : instr) (pgm : instr) (s : stack) (m : memory) : option (instr * stack * memory) := *)
+(*   match i with *)
+(*   | i1 ;; i2 => step_fun i1 (i2 ;; pgm) s m *)
+(*   (* the Nop case is not very satisfying *) *)
+(*   | Nop => Nonematch pgm with *)
+(*             | Nop => Some(Nop,s,m) *)
+(*             | Seq i1 i2 => step_fun i1 i2 s m *)
+(*           end *)
+(*   | If bt bf => if s is x::s then *)
+(*                  match x with *)
+(*                    | Dtrue => Some(bt ;; pgm,s,m) *)
+(*                    | Dfalse => Some(bf ;; pgm,s,m) *)
+(*                    | _ => None *)
+(*                  end else None *)
+(*   | Loop body => if s is x::s then *)
+(*                   match x with *)
+(*                   | Dtrue => Some(body ;;((Loop body) ;; pgm),s,m) *)
+(*                   | Dfalse => Some(pgm,s,m) *)
+(*                   | _ => None *)
+(*                   end else None *)
+(*   | Dip i1 => if s is x::s then *)
+(*                      match step_fun i1 pgm s m with *)
+(*                        | Some (i2,s',m') => Some(i2,x::s',m') *)
+(*                        | None => None *)
+(*                      end else None *)
+(*   | Drop => if s is x::xs then Some(pgm,xs,m) else None *)
+(*   | Dup => if s is x::xs then Some(pgm,x::x::xs,m) else None *)
+(*   | Swap => if s is x1::x2::s then Some(pgm,x2::x2::s,m) else None *)
+(*   | Push d => Some(pgm,d::s,m) *)
+(*   | Pair => if s is a::b::s then Some(pgm,(DPair a b)::s,m) else None *)
+(*   | Eq => if s is x::s then if is_comparable x then Some(pgm,(Int64 (get_eq x))::s,m) else None else None *)
+(*   | Neq => if s is x::s then if is_comparable x then Some(pgm,(Int64 (get_neq x))::s,m) else None else None *)
+(*   | Lt => if s is x::s then if is_comparable x then Some(pgm,(Int64 (get_lt x))::s,m) else None else None *)
+(*   | Not => if s is x::s then match x with *)
+(*                               | Dtrue => Some(pgm,Dfalse::s,m) *)
+(*                               | Dfalse => Some(pgm,Dtrue::s,m) *)
+(*                               | _ => None *)
+(*                             end else None *)
+(*   | And => if s is x1::x2::s then *)
+(*             if (is_bool x1 && is_bool x2) then *)
+(*               match (x1,x2) with *)
+(*                 | (Dtrue,Dtrue) => Some(pgm,Dtrue::s,m) *)
+(*                 | (_,_) => Some(pgm,Dfalse::s,m) *)
+(*               end else None *)
+(*           else None *)
+(*   | Or => if s is x1::x2::s then *)
+(*             if (is_bool x1 && is_bool x2) then *)
+(*               match (x1,x2) with *)
+(*                 | (Dtrue,_) => Some(pgm,Dtrue::s,m) *)
+(*                 | (_,Dtrue) => Some(pgm,Dtrue::s,m) *)
+(*                 | (_,_) => Some(pgm,Dfalse::s,m) *)
+(*               end else None *)
+(*           else None *)
+(*   | Mul => if s is x1::x2::s then *)
+(*             match (get_mul x1 x2) with *)
+(*               | Some(x12) => Some(pgm,x12::s,m) *)
+(*               | None => None *)
+(*           end else None *)
+(*   | Sub => if s is x1::x2::s then *)
+(*             match (get_sub x1 x2) with *)
+(*               | Some(x12) => Some(pgm,x12::s,m) *)
+(*               | None => None *)
+(*           end else None *)
+
+  (* | Le => if s is x::s then if is_comparable x then Some(pgm,(Int64 (get_le x))::s,m) else None else None *)
+  (* | Transfer_funds => if s is p::amount::contract::g::nil then *)
+  (*                 Some(pgm,[::get_return_value contract;get_new_global g],m) else None *)
+  (* | Now => Some(pgm,Timestamp (get_timestamp tt)::s,m) *)
+  (* | Balance => Some(pgm,DTez (get_current_amount tt)::s,m) *)
+  (* end. *)
+
+
+
+
+
+
+(* | Sub : instr *)
+(* . *)
+
+
+Fixpoint evaluate (i : instr) (s : stack) (m : memory) (f : nat) : option (stack * memory) :=
+  match f with
+  | 0 => Some(s,m)
+  | S f => match step_fun i s m with
+            | None => None
+            | Some(pgm',s',m') => evaluate pgm' s' m' f
+          end
+  end.
+
+Fixpoint evaluate_trace (i : instr) (s : stack) (m : memory) (trace : list stack) (f : nat) : option (list stack * memory) :=
+  match f with
+  | 0 => Some(trace,m)
+  | S f => if i is Nop then Some(trace,m) else match step_fun i s m with
+            | None => None
+            | Some(pgm',s',m') => evaluate_trace pgm' s' m' (s'::trace) f
+          end
+  end.
+
+
+Variable m : memory.
+
+Eval compute in evaluate factorial_program [::Int64 5] m 1000.
+Eval compute in evaluate_trace factorial_program [::Int64 5] m [::[::Int64 5]] 100.
+Eval compute in step_fun (DROP ;; DROP) [::Int8 1;Int8 1] m.
+Eval compute in step_fun (NOP ;; DROP) [::Int8 1] m.
+Eval compute in evaluate (DROP) [::Int8 1; Int8 1] m 1.
+Eval compute in evaluate (DROP ;; DROP) [::Int8 1;Int8 1] m 100.
+
+(* these lemmas need to be reformulated *)
+
+(* Lemma has_prog_type_cat : forall p q st1 st2 st3, *)
+(*   has_prog_type p (Pre_post st1 st2) -> *)
+(*   has_prog_type q (Pre_post st2 st3) -> *)
+(*   has_prog_type (p++q) (Pre_post st1 st3). *)
+(* Proof. *)
+(* elim => [|p ps Hps] q st1 st2 st3. *)
+(* - by move => Hnil; inversion Hnil. *)
+(* - by move => Hp Hq; inversion Hp; econstructor; eauto. *)
+(* Qed. *)
+
+(* Lemma step_fun_preserves_type i st1 st2 s m f : *)
+(*   has_prog_type i (Pre_post st1 st2) -> *)
+(*   has_stack_type s st1 -> *)
+(*   match (evaluate i s m f) with *)
+(*   | Some (s',m') => has_stack_type s' st2 *)
+(*   | None => True *)
+(*   end. *)
+(* Proof. *)
+(* move: f pgm st1 st2 s m. *)
+(* elim => [|f HIf] pgm st1 st2 s m //. *)
+(* case: pgm => [| i pgm] // . *)
+(*   by move => HPT; inversion HPT => HST //=. *)
+(* case: i => [| | (* Push *) d| | (* If *) bt bf| (* Loop *)body| (* Le *) | | |] /=. *)
+(* - case: s => [| x s]// . *)
+(*   move => HPT HST. *)
+(*   inversion HPT. *)
+(*   inversion HST. *)
+(*   inversion H2. *)
+(*   apply: HIf. *)
+(*     exact: H4. *)
+(*   rewrite -H8 in H10. *)
+(*   case: H10. *)
+(*     by move => _; rewrite -H11 => -> . *)
+(* - admit. (* TODO : Dup *) *)
+(* - admit. (* TODO : Push *) *)
+(* - admit. (* TODO : Pair *) *)
+(* - case: s => [| x s] //; case: x => // . *)
+(*   + move => HPT HST. *)
+(*     inversion HPT. *)
+(*     inversion H2. *)
+(*     apply: HIf. *)
+(*       apply: has_prog_type_cat. *)
+(*         exact: H12. *)
+(*         exact: H4. *)
+(*     rewrite -H7 in HST. *)
+(*     by inversion HST. *)
+(*   + move => HPT HST. *)
+(*     inversion HPT. *)
+(*     inversion H2. *)
+(*     apply: HIf. *)
+(*       apply: has_prog_type_cat. *)
+(*         exact: H13. *)
+(*         exact: H4. *)
+(*     rewrite -H7 in HST. *)
+(*     by inversion HST. *)
+(* - case: s => [| x s] //; case: x => // ; last first. *)
+(*   + move => HPT HST. *)
+(*     inversion HPT. *)
+(*     apply: HIf. *)
+(*       exact: H4. *)
+(*     inversion HST. *)
+(*     inversion H2. *)
+(*     rewrite -H8 in H11. *)
+(*     case: H11 => _. *)
+(*     by rewrite -H12 => -> . *)
+(*   + move => HPT HST. *)
+(*     inversion HPT. *)
+(*     inversion H2. *)
+(*     apply: HIf. *)
+(*       * apply: has_prog_type_cat. *)
+(*           exact: H10. *)
+(*         apply: PT_seq. *)
+(*           apply: IT_Loop. *)
+(*             exact: H9. *)
+(*           exact: H10. *)
+(*         exact: H4. *)
+(*       * inversion HST. *)
+(*         rewrite -H6 in H14. *)
+(*         case: H14 => _. *)
+(*           by rewrite -H7 => <- . *)
+(* - admit. (* TODO : Le *) *)
+(* - admit. (* TODO : Transfer_funds *) *)
+(* - admit. (* TODO : Now *) *)
+(* - admit. (* TODO : Balance *) *)
+(* Admitted. *)
+
+End Fun_semantics.
